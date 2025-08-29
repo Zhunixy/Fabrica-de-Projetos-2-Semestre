37ce1881ec2e4e0c1fb90671d1346cfbf356b421
Zhunixy <kauan8643@gmail.com>
2025-08-28 22:59:40 -0300

    melhorias front-end e uma logo provisoria

--- front_end/app/src/components/Layout/index.jsx
+++ front_end/app/src/components/Layout/index.jsx
@@ -1,33 +1,56 @@
-import { Link, Outlet } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom";
+import minhaImagem from "../../../assets/logo2.png";
 
 export function Menu() {
-    return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/Tecnica">Tecnica</Link>
-                </li>
-            </ul>
-        </nav>)
+  return (
+    <nav>
+      <ul>
+        <li>
+          <img className="logo" src={minhaImagem} alt="Logo" />
+        </li>
+
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+
+        <li>
+          <Link to="/">Pagamentos</Link>
+        </li>
+
+        <li>
+          <Link to="/">Verificação</Link>
+        </li>
+
+        <li>
+          <Link to="/Tecnica">Sobre</Link>
+        </li>
+
+        <li className="login">
+          <Link to="/">Login</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export function Footer() {
-    return <footer>
-        <p>{new Date().getFullYear()} - Todos os direitos reservados &copy;</p>
+  return (
+    <footer className="footer">
+      <p>{new Date().getFullYear()} - Todos os direitos reservados &copy;</p>
     </footer>
+  );
 }
 
 export function Layout() {
-    return <>
-        <header>
-            <Menu />
-        </header>
-        <main>
-            <Outlet />
-        </main>
-        <Footer />
+  return (
+    <>
+      <header>
+        <Menu />
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
     </>
+  );
 }
--- front_end/app/src/pages/Home/index.jsx
+++ front_end/app/src/pages/Home/index.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 export default function HomePage() {
   return (
     <>
-      <div>
-        <h1>Bem-vindo ao Meu Site</h1>
-        <p>Esta é a página inicial do projeto em React.</p>
+      <div className="main">
+        <h1>Bem-vindo ao sistema verificador de boletos (Usuario)</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, nobis.</p>
+        <button className="btn">Confira Agora</button>
       </div>
     </>
   );
