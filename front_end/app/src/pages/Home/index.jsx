c02a30a1f2fe4cbb16bdacb9c14cac15b8b90971
Zhunixy <kauan8643@gmail.com>
2025-08-29 22:21:00 -0300

    melhorias na responsividade do site

--- front_end/app/src/pages/Home/index.jsx
+++ front_end/app/src/pages/Home/index.jsx
@@ -7,12 +7,12 @@ export default function HomePage() {
         </h1>
         <hr className="hr" />
         <p>
-          <i class="fa-solid fa-circle-info"></i> Gerencie, valide e envie os
+          <i className="fa-solid fa-circle-info"></i> Gerencie, valide e envie os
           seus boletos com facilidade <span>Usuario</span>
         </p>
         <button className="btn">Confira Agora</button>
         <div className="seta">
-          <i class="fa-solid fa-arrow-down"></i>
+          <i className="fa-solid fa-arrow-down"></i>
         </div>
       </div>
     </>
