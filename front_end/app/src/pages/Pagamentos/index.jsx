b0ba4d4485c68e39b5cfbef6677799bbfcfca78e
dolphin4748 <matheusluciocx5726@gmail.com>
2025-09-05 13:06:19 -0300

    adcionando modal basica na pagina de boletos

--- front_end/app/src/pages/Pagamentos/index.jsx
+++ front_end/app/src/pages/Pagamentos/index.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import "./index.css";
+import Modal from "../../components/Modal";
 
 export default function PagamentoPage() {
   const [boletos, setBoletos] = useState([
     {
+      id: 1,
       nBoleto: "23323",
       cpf: "121212122",
       emissao: "19/06/2000",
@@ -11,6 +13,7 @@ export default function PagamentoPage() {
       valor: 800
     },
     {
+      id: 2,
       nBoleto: "18219",
       cpf: "121212122",
       emissao: "19/06/2000",
@@ -18,6 +21,7 @@ export default function PagamentoPage() {
       valor: 800
     },
     {
+      id: 3,
       nBoleto: "43255",
       cpf: "121212122",
       emissao: "19/06/2000",
@@ -25,6 +29,7 @@ export default function PagamentoPage() {
       valor: 800
     },
     {
+      id: 4,
       nBoleto: "43256",
       cpf: "121212122",
       emissao: "19/06/2000",
@@ -32,6 +37,7 @@ export default function PagamentoPage() {
       valor: 800
     },
     {
+      id: 5,
       nBoleto: "98766",
       cpf: "121212122",
       emissao: "19/06/2000",
@@ -39,6 +45,7 @@ export default function PagamentoPage() {
       valor: 800
     },
     {
+      id: 6,
       nBoleto: "54361",
       cpf: "121212122",
       emissao: "19/06/2000",
@@ -48,6 +55,13 @@ export default function PagamentoPage() {
   ]);
 
   const [search, setSearch] = useState("");
+  const [selected, setSelected] = useState(null);
+  const [openModal, setOpenModal] = useState(false);
+
+  const selecionarLinha = async (divida) => {
+    setSelected(divida);
+    setOpenModal(true);
+  };
 
   return (
     <>
@@ -57,6 +71,7 @@ export default function PagamentoPage() {
           Lorem ipsum dolor sit amet consectetur adipisicir non? Recusandae quo
           iste mollitia?
         </p>
+        <p>{selected?.id}</p>
         <div className="form">
           <input type="text" className="search" placeholder="Buscar"
                 value={search}
@@ -83,22 +98,56 @@ export default function PagamentoPage() {
           {/* Lógica de inserir o dicionario de boletos atraves de uma função map como se fosse um for, e inserir o componente */}
           <tbody>
             {boletos.map((boleto) => (
-              <LinhaBoleto boleto={boleto}> </LinhaBoleto>
+              <LinhaBoleto key={boleto.id} 
+                  boleto={boleto}
+                  onClick={() => selecionarLinha(boleto)}> </LinhaBoleto>
             ))}
           </tbody>
         </table>
+        {/* paginação ainda inexistente */}
         <div className="pagination">
           <div className="prox">Próximo</div>
           <div className="ante">Próximo</div>
         </div>
+
+        {/* Modal do boleto */}
+        <Modal open={openModal}>
+            <form>
+                <div className="column">
+                    <div className="row">
+                      <label>Numero do boleto:</label>
+                      <input defaultValue={selected?.nBoleto} required type="text"/>
+                    </div>
+                    <div className="row">
+                      <label>CNJ/CPF:</label>
+                      <input defaultValue={selected?.cpf} required type="text"/>
+                    </div>
+                    <div className="row">
+                      <label>Emissão</label>
+                      <input defaultValue={selected?.emissao} required type="text"/>
+                    </div>
+                    <div className="row">
+                      <label>Vencimento:</label>
+                      <input defaultValue={selected?.vencimento} required type="text"/>
+                    </div>
+                    <div className="row">
+                      <label >Valor:</label>
+                      <input defaultValue={selected?.valor} required type="number" />
+                    </div>
+                    <div className="row-end">
+                        <button type="reset" onClick={() => setOpenModal(false)}>Cancelar</button>
+                    </div>
+                </div>
+            </form>
+        </Modal>
       </div>
     </>
   );
 }
 
-function LinhaBoleto({ boleto }) {
+function LinhaBoleto({ boleto, onClick }) {
   return (
-    <tr>
+    <tr onClick={onClick} >
       <td>{boleto.nBoleto}</td>
       <td>{boleto.cpf}</td>
       <td>{boleto.emissao}</td>
