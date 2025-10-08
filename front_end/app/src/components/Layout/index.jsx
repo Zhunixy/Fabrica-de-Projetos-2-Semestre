d44bef93350a43231c6dd9dfff105553f84be9ff
matheus lucio <151792207+dolphin4748@users.noreply.github.com>
2025-10-08 20:12:32 -0300

    Update index.jsx

--- front_end/app/src/components/Layout/index.jsx
+++ front_end/app/src/components/Layout/index.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState, useReducer, useMemo } from "react";
-import { Link, Outlet, useNavigate } from "react-router-dom";
+import { Link, Outlet, useNavigate, useLocation } from "react-router-dom";
 import minhaImagem from "../../../assets/logo2.png";
 
 export function Menu({ logado, setLogado }) {
   const [navOpen, setNavOpen] = useState(false);
-  const [width, setWidth] = useState(window.innerWidth);
   const navigate = useNavigate();
+  const pagina = useLocation().pathname;
 
   useEffect(() => {
     function handleResize() {
@@ -16,44 +16,35 @@ export function Menu({ logado, setLogado }) {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  useEffect(() => {
-    // sempre que o width mudar, decide se abre/fecha o menu
-    if (width > 768) {
-      setNavOpen(true);
-    }
-  }, [width]);
-
   return (
     <nav>
       <div className="row">
         <img
-          className={`logo ${width > 768 ? "hidden" : ""}`}
+          className={"logo"}
           onClick={() => {
-            logado ? navigate("/") : navigate("/login");
+            logado ? setNavOpen(!navOpen) : navigate("/login");
           }}
           src={minhaImagem}
           alt="Logo"
         />
-        <button
-          className={`btn-nav row-end ${width > 768 ? "hidden" : ""}`}
-          onClick={() => {
-            setNavOpen(!navOpen);
-          }}
+        <li
+          className={`login ${
+            pagina === "/Login" || pagina === "/Cadastro" ? "hidden" : ""
+          }`}
+          style={{ listStyle: "none" }}
         >
-          <i class="icon fa-solid fa-list"></i>
-        </button>
+          <Link
+            className="ex"
+            to="/Login"
+            onClick={() => {
+              logado ? setLogado(false) : "";
+            }}
+          >
+            {logado ? "Logout" : "Login"}
+          </Link>
+        </li>
       </div>
       <ul className={navOpen ? "" : "hidden"}>
-        <li>
-          <img
-            className={`logo ${width > 768 ? "" : "hidden"}`}
-            onClick={() => {
-              logado ? navigate("/") : navigate("/login");
-            }}
-            src={minhaImagem}
-            alt="Logo"
-          />
-        </li>
         <li className={logado ? "" : "hidden"}>
           <Link to="/">
             <i className="icon fa-solid fa-house"></i>Home
@@ -67,17 +58,6 @@ export function Menu({ logado, setLogado }) {
         <li className={logado ? "" : "hidden"}>
           <Link to="/Tecnica">
             <i className="icon fa-solid fa-folder"></i>Sobre
-          </Link>
-        </li>
-        <li className={`login ${logado ? "" : "hidden"}`}>
-          <Link
-            className="ex"
-            to="/Login"
-            onClick={() => {
-              logado ? setLogado(false) : "";
-            }}
-          >
-            {logado ? "Logout" : "Login"}
           </Link>
         </li>
       </ul>
