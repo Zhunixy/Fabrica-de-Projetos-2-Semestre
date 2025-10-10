8e17397551ac4d84bf70738501d988642329fbeb
dolphin4748 <matheusluciocx5726@gmail.com>
2025-10-10 12:24:34 -0300

    pequenas alterações na navbar

--- front_end/app/src/App.jsx
+++ front_end/app/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import { Layout } from './components/Layout';
+import Layout from './components/Layout';
 import HomePage from './pages/Home';
 import TecnicaPage from './pages/Tecnica';
 import PagamentoPage from './pages/Pagamentos';
--- front_end/app/src/components/Layout/index.jsx
+++ front_end/app/src/components/Layout/index.jsx
@@ -7,15 +7,6 @@ export function Menu({ logado, setLogado }) {
   const navigate = useNavigate();
   const pagina = useLocation().pathname;
 
-  useEffect(() => {
-    function handleResize() {
-      setWidth(window.innerWidth);
-    }
-    window.addEventListener("resize", handleResize);
-
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
   return (
     <nav>
       <div className="row">
@@ -27,39 +18,21 @@ export function Menu({ logado, setLogado }) {
           src={minhaImagem}
           alt="Logo"
         />
-        <li
-          className={`login ${
-            pagina === "/Login" || pagina === "/Cadastro" ? "hidden" : ""
-          }`}
-          style={{ listStyle: "none" }}
-        >
+
+        <li className={`login ${pagina === "/Login" || pagina === "/Cadastro" ? "hidden" : ""}`}
+            style={{ listStyle: "none" }}>
           <Link
             className="ex"
             to="/Login"
-            onClick={() => {
-              logado ? setLogado(false) : "";
-            }}
-          >
-            {logado ? "Logout" : "Login"}
+            onClick={() => {logado ? setLogado(false) : "";}}>
+              {logado ? "Logout" : "Login"}
           </Link>
         </li>
       </div>
-      <ul className={`menuAnima ${navOpen ? "open" : "closed"}`}>
-        <li className={logado ? "" : "hidden"}>
-          <Link to="/">
-            <i className="icon fa-solid fa-house"></i>Home
-          </Link>
-        </li>
-        <li className={logado ? "" : "hidden"}>
-          <Link to="/Pagamentos">
-            <i className="icon fa-solid fa-bag-shopping"></i>Pagamentos
-          </Link>
-        </li>
-        <li className={logado ? "" : "hidden"}>
-          <Link to="/Tecnica">
-            <i className="icon fa-solid fa-folder"></i>Sobre
-          </Link>
-        </li>
+      <ul className={`menuAnima ${navOpen && logado ? "open" : "closed"}`}>
+        <li><Link to="/"><i className="icon fa-solid fa-house"></i>Home</Link></li>
+        <li><Link to="/Pagamentos"><i className="icon fa-solid fa-bag-shopping"></i>Pagamentos</Link></li>
+        <li><Link to="/Tecnica"><i className="icon fa-solid fa-folder"></i>Sobre</Link></li>
       </ul>
     </nav>
   );
