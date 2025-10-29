0b1f9f2e55d1978cf2e941805efcf3c5641dbaf9
dolphin4748 <matheusluciocx5726@gmail.com>
2025-10-29 15:30:53 -0300

    mais correções pequenas

--- back_end/services/getDados.php
+++ back_end/services/getDados.php
@@ -1,8 +1,14 @@
 <?php
 
 header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+
+// Intercepta requisição OPTIONS e retorna 200 direto
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 require_once "../classes/Crud.php";
 
