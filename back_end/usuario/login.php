053795cdebd4e8f664049dd47dae9b99879ceb19
matheus lucio <151792207+dolphin4748@users.noreply.github.com>
2025-10-29 21:48:32 -0300

    Update CORS headers to allow credentials
    Added support for credentials in CORS headers.

--- back_end/usuario/login.php
+++ back_end/usuario/login.php
@@ -3,6 +3,7 @@
 header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Credentials: true");
 
 require_once "../classes/Usuario.php";
 
@@ -12,4 +13,5 @@ $login = $input['login'] ?? '';
 $senha = $input['senha'] ?? '';
 
 $usuario = new Usuario([], 'usuario');
+
 echo json_encode($usuario->login($login, md5($senha)));
