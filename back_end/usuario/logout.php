557277c50856811b69078642c510472c1cc320f9
matheus lucio <151792207+dolphin4748@users.noreply.github.com>
2025-10-29 21:48:19 -0300

    Enhance CORS headers in logout.php
    Added support for credentials in CORS headers.

--- back_end/usuario/logout.php
+++ back_end/usuario/logout.php
@@ -3,8 +3,10 @@
 header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Credentials: true");
 
 require_once '../classes/Usuario.php';
 
 $usuario = new Usuario([], '');
+
 echo json_encode($usuario->logout());
